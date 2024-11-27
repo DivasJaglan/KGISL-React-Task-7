4747aa347075c6bbaf845f8832d777f71807bae3
DivasJaglan <divasjaglan9@gmail.com>
2024-11-27 21:08:08 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App d-flex justify-content-center align-items-center">
       <div className="container w-25 text-center border border-3 border-opacity-25  border-black rounded-4 d-flex flex-column gap-3">
-        <h1>Guess the Number</h1>
+        <h2>Guess the Number</h2>
 
         <input
           className="text-center w-25 align-self-center"
@@ -31,13 +31,13 @@ function App() {
           value={guessdNumber}
           onChange={(e) => setGuessdNumber(+e.target.value)}
         />
-        <h4
+        <h6
           className={`${
             feedback === "Correct" ? "bg-success" : "bg-danger"
           } w-50 align-self-center p-1 rounded-3`}
         >
           {feedback}
-        </h4>
+        </h6>
         <div className="d-flex justify-content-around p-2">
           <button className="rounded-3" onClick={generateRandomNumber}>
             Generate Number
